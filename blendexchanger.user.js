20ed15fef8a2f8265d550d0308ae3d39c95133e5
gandalf3 <gandalf3@blendermonkey.com>
2019-05-30 18:29:36 -0700

    Remove jquery
    Also some minor readability and style tweaks

--- blendexchanger.user.js
+++ blendexchanger.user.js
@@ -1,112 +1,113 @@
 // ==UserScript==
 // @name        BlendExchanger
 // @namespace   blender.org
-// @description Upload .blends to Blend-Exchange directly from StackExchange
+// @description Upload .blends to Blend-Exchange without leaving StackExchange
 // @include     *blender.stackexchange.com/*
-// @version     1
-// @grant       none
+// @version     2.0
 // ==/UserScript==
 
-//CONFIG URL - I put it here where it is easy to change
-var urlEmbed = "http://blend-exchange.giantcowfilms.com/embedUpload/?qurl=" + document.URL;
+// Shortcut is Alt+B by default, you can change it here if you wish
+function shortcutShouldFire(ev) {
+   return (ev.altKey && (ev.key == 'b'));
+}
+
+// URL of blend-exchange embeded upload view
+const EmbedURL = "https://blend-exchange.giantcowfilms.com/embedUpload/?qurl=" + document.URL;
 
 //Quiet debugging prints
 console.log = function() {};
 
-//stuff in main is run with jquery:
+
 function main() {
-   var pref_extra_markdown = 0
    console.log("running main!");
-   
+
    function startInjection() {
-      
-      //add blend button when any of these elements are clicked:
-      $(document).on('click', 'a.edit-post', waitForButtonRow); //inline editing
-      $(document).on('click', 'input[value="Add Another Answer"]', waitForButtonRow); //adding multiple answers
-      //review editing:
-      $(document).on('click', 'input[value="Improve"]', waitForButtonRow); //improving suggested edits
-      $(document).on('click', 'input[value="Edit"]', waitForButtonRow); //editing close voted questions
-      
-      //define keyboard shortcut even handler (Alt+B)
-      $(document).on('keydown', "textarea.wmd-input", function (e) {
-         if (e.altKey && (e.which === 66)) {
-            insertBlendFileDialog(this, urlEmbed);
-         }
+
+     	// We can't know the url of the question if it hasn't been asked yet, so don't just abort
+     	if (document.URL.indexOf("blender.stackexchange.com/questions/ask") >= 1) {
+         console.log('Disabling BlendExchanger because you\'re currently asking a question.' +
+                    'Come back and edit it once you\'ve posted it (so that we know the URL)');
+         return;
+      }
+
+      //try and add .blend button when any of these elements are clicked:
+      document.querySelectorAll('a.edit-post, ' +
+                                'input[value="Add Another Answer"], ' + //adding multiple answers
+                                'input[value="Improve"], ' + //improving suggested edits
+                                'input[value="Edit"]' //editing close voted questions
+                               ).forEach(elem => {
+      	elem.addEventListener('click', waitForButtonRow);
       });
-      
+
+      //define keyboard shortcut event handler (Alt+B)
+      document.querySelectorAll('textarea.wmd-input').forEach(elem => {
+         elem.addEventListener('keydown', ev => {
+            if (shortcutShouldFire(ev)) {
+               ev.preventDefault();
+               insertBlendFileDialog(this, EmbedURL);
+            }
+         });
+      });
+
       waitForButtonRow();
    }
-   
+
+   // Repeatedly check for button rows and inject buttons into every one we find
    function waitForButtonRow() {
       console.log("waiting for button row..");
-      
-      function testForButtonRow() { //test for a .wmd-button-row every half a second until one is found
-         if (counter < 60) {
-            if ($(".wmd-button-row").length > 0) { //if button row(s) exist, test each one to see if it already has a blend button
-               console.log("found .wmd-button-row");
-               $(".wmd-button-row").each(function() {console.log("does it have a blend button? ", $(this).has(".wmd-blend-button").length);console.log("id", $(this).attr("id"))});
-               $(".wmd-button-row").each(function() {
-                  if ($(this).has(".wmd-blend-button").length == 0) { //if no blend button exists, inject one
-                     
-                     if (document.URL.indexOf("blender.stackexchange.com/questions/ask") < 1) { //but only add button if we are not asking a question
-                        console.log("does not contain blend button, inserting one");
-                        injectButton($(this));
-                     }
-                     else {
-                        console.log("no buttons allowed on ask question page");
-                     }
-                  }
-               });
-            }
-            else {
-               setTimeout(testForButtonRow, 500);
-               counter++;
-            }
-         }
-         else {
+
+      function testForButtonRow() {
+         if (counter > 60) {
             console.log("did not find a place to put blend button within 30 seconds. giving up.");
             return;
          }
+
+         let button_rows = document.getElementsByClassName('wmd-button-row');
+
+         if (button_rows.length <= 0) {
+            setTimeout(testForButtonRow, 500);
+            counter++;
+            return;
+         }
+
+         for (button_row of button_rows) {
+            //if no blend button already exists, inject one
+            if (button_row.getElementsByClassName("wmd-blend-button").length == 0) {
+               injectButton(button_row);
+            }
+         }
       }
-      
+
       var counter = 0;
-      setTimeout(testForButtonRow, 500); //bit of spacer time to allow SE js to execute and add button rows.
-      //TODO: This causes a potential race condition (if SE js takes longer than 500ms), a better workaround would be nice..
+      setTimeout(testForButtonRow, 500);
    }
-   
+
    function injectButton(buttonRow) {
-      
-      var blendButtonId = 'wmd-blend-button' + buttonRow.attr("id").replace(/[^0-9]+/g, "");
-      
-      var li = $("<li/>");
-      li.attr('id', blendButtonId);
-      li.attr('title', 'Upload .blend to Blend-Exchange Alt+B');
-      li.addClass('wmd-button wmd-blend-button');
-      
-      var click_timeout = false;
-      li.click(function() {
-         if (click_timeout == false) { //prevent user from clicking while popup is being created by means of a 500ms timeout (a potential race condition :/)
-            click_timeout = true;
-            insertBlendFileDialog($(this).parents("div[class='wmd-container']").find("textarea").first()[0],urlEmbed);
-            setTimeout(function() { click_timeout = false }, 500);
-         }
-         else {
-            console.log("Too many clicks, too fast. Not doing anything");
+      var blendButtonId = 'wmd-blend-button' + buttonRow.getAttribute("id").replace(/[^0-9]+/g, "");
+
+      var li = document.createElement('li');
+      li.setAttribute('id', blendButtonId);
+      li.setAttribute('title', 'Upload .blend to Blend-Exchange (Alt+B)');
+      li.classList.add('wmd-button', 'wmd-blend-button');
+
+      var respond_to_click = true;
+      li.addEventListener('click', () => {
+         //prevent user from clicking too many times too quickly; ignore secondary clicks which occur within 500ms
+        console.log(respond_to_click);
+         if (respond_to_click == true) {
+            respond_to_click = false;
+            insertBlendFileDialog(li.parentNode.querySelector("div[class='wmd-container'] textarea"), EmbedURL);
+            setTimeout(function() { respond_to_click = true }, 500);
          }
       });
-      
-      //shuffle existing buttons around so blend button is the one after image button
-      var imgButton = $(buttonRow).children("[id^=wmd-image]");
-      li.insertAfter(imgButton);
-      
-      li.css("left", parseInt(imgButton.css("left")) + 25 + "px"); //put blend button 25 px after img button
-      li.nextAll().each(function() {
-         $(this).css("left", parseInt($(this).css("left")) + 25 + "px"); //move buttons after blend button farther over
-      });
-      
+
+      //insert out button after the image button
+      let image_button = buttonRow.querySelector("#wmd-image-button");
+      buttonRow.insertBefore(li, image_button.nextSibling);
+
       //Add image element with base64 encoded png icon
-      var img = $("<img/>").appendTo(li);
-      img.attr('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V%2B0%2FAAABaFBMVEUAAAAAAA\
+      let icon = document.createElement('img');
+      icon.setAttribute('src','data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V%2B0%2FAAABaFBMVEUAAAAAAA\
       BUpudZo%2BD9%2Ff0BbMcKfdp1wO9bpOBNoeNClts3jdQ3kdgje8hbqt00jdjW1tYAe94dh9wAbca3t7f09PTQ0ND\
       y8vLOzs7RaBPo6Oj3s3LTdSdotO%2FIyMj1rWlKdqz3sGrRbBr0eAD%2F%2F%2F%2F8%2BvmB0PmAzPhbpOCxw9tIi\
       sb028XtzLEzZKLahDv2ljrPZxKZ5f%2BW4v%2F9%2Ff2N2f16xvZvvPJRr%2BQzmtpJltJElNDr2Mjv1L5Hhr67u7sx\
@@ -118,23 +119,23 @@ function main() {
       FCl%2FkYExlwmnj7ZTyj873o%2BqGfYRLh%2FglzptJAaDdULs7MJZh4LGxzYlQge3Xv48rFY2me8QdttnGCliFfqTz5OFXlp\
       OVFf3AKz7yAPGzUXuWQyoWk5EogSOAgC8%2Bq2od4FPXt5JMbgZ4WPgoylYXapyh%2Bi%2Bm1NMb2RopQf9N1fSmVifd11ly9F\
       ppN4zSV6R3tIKBazW6NDE3%2BR4rGH9wFCTM7PoILZz52y34AnZsz2CzgIFUAAAAASUVORK5CYII%3D');
-      
+
+     	li.appendChild(icon);
    }
-   
+
    function insertBlendFileDialog(txta, url) {
-      
-      //for easy adjustment of popup size
+      //popup size
       var popupWidth = 640;
       var popupHeight = 400;
-      
-      var left = (($(window).width()/2)+window.screenX)-(popupWidth/2);
-      var top = (($(window).height()/2)+window.screenY)-(popupHeight/2);
-      
+
+      var left = ((window.outerWidth/2)+window.screenX)-(popupWidth/2);
+      var top = ((window.outerHeight/2)+window.screenY)-(popupHeight/2);
+
       console.log("left", left);
       console.log("top", top);
-      
+
       var blendUploadWindow = window.open(url, "Blend-Exchange wormhole portal vortex uploader thingy", "width=" + popupWidth + ",height=" + popupHeight + ",toolbar=no,menubar=no,location=no,status=no,scrollbars=no,resizable=no,left=" + left + ",top=" + top);
-      
+
       window.addEventListener("message", function (event) {
          //It is necessary to check the origin form stopping foreign pages hijacking the event
          if (event.data.name == "embedSource" && event.origin == "http://blend-exchange.giantcowfilms.com") {
@@ -154,41 +155,41 @@ function main() {
          }
       });
    };
-   
+
    function insertBlendFile(txta,embedCode) {
       console.log(embedCode);
-      
+
       if (txta.selectionStart == null) return;
       //embed insertion getting fired multipole times investigate
       var start = txta.selectionStart;
       var end = txta.selectionEnd;
       var chars = txta.value;
       console.log("chars: " + chars);
-      
+
       //separate selection from rest of body
       var pre = chars.slice(0, start);
       var post = chars.slice(end);
-      
+
       //put everything back together again
       txta.value = pre + embedCode + post;
-      
+
       //highlight newly added embed code
       txta.selectionStart = pre.length;
       txta.selectionEnd = pre.length + embedCode.length;
-      
-      $(txta).focus();
-      
+
+      txta.focus();
+
       updateMarkdownPreview(txta);
-      
+
    }
-   
+
    //function to force update the live markdown render
    function updateMarkdownPreview(element) {
-      
+
       var keyboardEvent = document.createEvent("KeyboardEvent");
       var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-      
-      
+
+
       //horrible hack so undo after inserting blend tags only removes blend tags
       keyboardEvent[initMethod](
          "keydown", // event type : keydown, keyup, keypress
@@ -216,40 +217,10 @@ function main() {
          0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
       );
       element.dispatchEvent(keyboardEvent);
-      
+
    }
-   
-   
+
    startInjection() //call initial startup function
 }
+
 main();
-
-//get jquery on chrome, thanks to this SO post: http://stackoverflow.com/a/12751531/2730823
-if (typeof jQuery === "function") {
-   console.log ("Running with local copy of jQuery!");
-   main (jQuery);
-}
-else {
-   console.log ("fetching jQuery from some 3rd-party server.");
-   add_jQuery (main, "1.7.1");
-}
-
-function add_jQuery (callbackFn, jqVersion) {
-   var jqVersion   = jqVersion || "1.7.1";
-   var D           = document;
-   var targ        = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
-   var scriptNode  = D.createElement ('script');
-   scriptNode.src  = 'http://ajax.googleapis.com/ajax/libs/jquery/'
-   + jqVersion
-   + '/jquery.min.js'
-   ;
-   scriptNode.addEventListener ("load", function () {
-      var scriptNode          = D.createElement ("script");
-      scriptNode.textContent  =
-      'var gm_jQuery  = jQuery.noConflict (true);\n'
-      + '(' + callbackFn.toString () + ')(gm_jQuery);'
-      ;
-      targ.appendChild (scriptNode);
-   }, false);
-   targ.appendChild (scriptNode);
-}
